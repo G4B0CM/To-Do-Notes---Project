118ed76e287e5c3a1bad3c81d3d7c9664c7b19fd
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-11-21 01:39:35 -0500

    Arreglando algunos ajustes dentro de lso CRUD y el cambio de base de datos a SQLite. reparare un error en NoteCreate

--- backend/to_do_project/core/entites/note.py
+++ backend/to_do_project/core/entites/note.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 
+#Notas 
+class Note(BaseModel):
+    #Creamos una nota(clase) con id integer, titulo string, contenido string y fecha de creacion datetime
+    id: int
+    title: str
+    note_content: str
+    creation_date: datetime.now()
+
+
 class NoteBase(BaseModel):
     title : str = Field(...,min_length=10, max_length=50)
     note_content : str = Field(..., min_length=10)
@@ -8,7 +17,9 @@ class NoteBase(BaseModel):
     is_completed : bool = Field(default=False)
 
 class NoteCreate(NoteBase):
-    pass
+    content : str #Contenido de la nota, obviamete es un string
+    creation_date : datetime = Field(default_factory=datetime.now) #Tomamos la fecha actual al crear la nota
 
 class NoteRead(NoteBase):
+    #Esta función solo deberia de tomar el numero entero para buscar la nota por su id
     id : int
